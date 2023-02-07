2dca5e4a677c741d6d75b6f904d634fe3ed0fa62
Jackson Price <price.jacksonk@gmail.com>
2023-02-07 14:56:42 -0600

    untrack files in gitignore

--- app/models/concerns/youtube_dl_config.rb
+++
@@ -1,7 +0,0 @@
-module YoutubeDlConfig
-  extend ActiveSupport::Concern
-  def build_command(conversion_id, url)
-    "youtube-dl -o \"/home/jackson/gitrepos/vert_api/public/user_downloads/#{conversion_id}.%(ext)s\" --extract-audio --audio-format mp3 #{url}"
-  end
-
-end
