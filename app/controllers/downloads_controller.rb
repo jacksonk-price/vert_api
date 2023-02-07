0dfad88332027261fecfec9cd41e984584358bb7
Jackson Price <price.jacksonk@gmail.com>
2023-02-07 14:50:57 -0600

    Add: functionality for convert action

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -5,7 +5,38 @@ class DownloadsController < ApplicationController
   require 'colorize'
   require 'open-uri'
 
-  def convert
+  include YoutubeValidation
 
+  def convert
+    conversion_id = SecureRandom.uuid
+    Rails.logger.info "Created conversion id #{conversion_id}".green
+
+    url = params[:inputurl]
+
+    if valid_url?(url)
+      download = Download.new
+      download.conversion_id = conversion_id
+      download.ip = request.remote_ip
+      download.video_url = url
+
+      download.start_conversion = Time.now
+      Rails.logger.info "Beginning conversion for #{url} with conversion id: #{conversion_id}...".yellow
+      status = download.convert_url
+      download.end_conversion = Time.now
+
+      if status.success?
+        download.save!
+        file_string = File.read("public/user_downloads/#{conversion_id}.mp3")
+        encoded_file = Base64.encode64(file_string)
+        Rails.logger.info "The audio has been downloaded successfully".green
+        File.delete("public/user_downloads/#{conversion_id}.mp3")
+        render json: { status: '200', message: 'Video successfully converted to audio.', mp3_base: encoded_file }
+      else
+        puts "An error occurred while converting the video."
+        render json: { status: '500', message: 'Something went wrong during conversion. Please try again'}
+      end
+    else
+      render json: { status: '422', message: 'Url is invalid' }
+    end
   end
 end
