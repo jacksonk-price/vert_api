cab73d6a3408adacc3ceb9335b27df3f752febdb
Jackson Price <price.jacksonk@gmail.com>
2023-02-07 14:48:35 -0600

    Add: convert_url method to download model

--- app/models/download.rb
+++ app/models/download.rb
@@ -1,2 +1,8 @@
 class Download < ApplicationRecord
+  include YoutubeDlConfig
+  def convert_url
+    cmd = build_command(conversion_id, video_url)
+    stdout, stderr, status  = Open3.capture3(cmd)
+    status
+  end
 end
