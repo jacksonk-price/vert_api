59a39851a1e1dfe26c37de5046d6b2964def79dd
Jackson Price <price.jacksonk@gmail.com>
2023-03-31 19:48:00 -0500

    Add: YtDlpDownload class

---
+++ lib/yt_dlp_downloader.rb
@@ -0,0 +1,28 @@
+class YtDlpDownloader
+  attr_accessor :url, :format
+  def initialize(url, format)
+    @url = url
+    @format = format
+  end
+
+  def extract_audio
+    output, status = Open3.capture2(extract_audio_command)
+    { audio: output, status: status }
+  end
+
+  def get_video_title
+    output, status = Open3.capture2(extract_title_command)
+    { title: output, status: status }
+  end
+
+  private
+
+  def extract_audio_command
+    "yt-dlp -f 'bestaudio[ext=#{@format}]' --no-continue -o - #{@url}"
+  end
+
+  def extract_title_command
+    "yt-dlp --get-title #{@url}"
+  end
+
+end
