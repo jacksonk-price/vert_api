f81bdcddd50fe31d21d8f5b9050123c591eb21fe
Jackson Price <price.jacksonk@gmail.com>
2023-03-31 19:50:07 -0500

    Refactor: functionality moved out into separate classes

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,42 +1,29 @@
 class DownloadsController < ApplicationController
   require 'open3'
-  require 'securerandom'
-  require 'stringio'
   require 'colorize'
-  require 'open-uri'
 
   include YoutubeValidation
 
   def convert
-    conversion_id = SecureRandom.uuid
-    Rails.logger.info "Created conversion id #{conversion_id}".green
-
     url = params[:inputurl]
 
     if valid_url?(url)
-      download = Download.new
-      download.conversion_id = conversion_id
-      download.ip = request.remote_ip
-      download.video_url = url
+      download = Download.new(ip: request.remote_ip, video_url: url)
 
       download.start_conversion = Time.now
-      Rails.logger.info "Beginning conversion for #{url} with conversion id: #{conversion_id}...".yellow
-      status = download.convert_url
+      result = download.convert_url
       download.end_conversion = Time.now
+      puts result[:title]
 
-      if status.success?
+      if result[:status].success?
         download.save!
-        file_string = File.read("public/user_downloads/#{conversion_id}.mp3")
-        encoded_file = Base64.encode64(file_string)
-        Rails.logger.info "The audio has been downloaded successfully".green
-        File.delete("public/user_downloads/#{conversion_id}.mp3")
-        render json: { status: 'success', message: 'Video successfully converted to audio.', mp3_base: encoded_file }
+
+        render json: { status: 'success', message: 'Video successfully converted to audio.', video_title: result[:title], wav_base64: result[:output]}
       else
-        puts "An error occurred while converting the video."
-        render json: { status: 'error', message: 'Something went wrong during conversion'}
+        render json: { status: 'error', message: 'Something went wrong during conversion.'}
       end
     else
-      render json: { status: 'error', message: 'Url is invalid' }
+      render json: { status: 'error', message: 'Url is invalid.' }
     end
   end
 end
--- app/models/download.rb
+++ app/models/download.rb
@@ -1,8 +1,18 @@
 class Download < ApplicationRecord
-  include YoutubeDlConfig
+  require 'yt_dlp_downloader'
+  require 'ffmpeg_converter'
+
   def convert_url
-    cmd = build_command(conversion_id, video_url)
-    stdout, stderr, status  = Open3.capture3(cmd)
-    status
+    # youtube provides m4a audio format.
+    # convert to m4a format first
+    ytdlp = YtDlpDownloader.new(video_url, 'm4a')
+    yt_dlp_audio = ytdlp.extract_audio
+    yt_dlp_title = ytdlp.get_video_title
+      # then convert to wav
+    wav_result = FfmpegConverter.new(yt_dlp_audio[:audio]).convert_to_wav
+    # encode wav binary to base64
+    encoded_wav = Base64.encode64(wav_result[:wav_output])
+
+    { output: encoded_wav, title: yt_dlp_title[:title], status: wav_result[:status] }
   end
 end
