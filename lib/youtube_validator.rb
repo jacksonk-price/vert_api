98d0abce820dfbbf3b2deee87c39ac7d5f06c632
Jackson Price <price.jacksonk@gmail.com>
2023-08-06 17:09:36 -0500

    update: regex for validating url

--- lib/youtube_validator.rb
+++ lib/youtube_validator.rb
@@ -1,6 +1,6 @@
 module YoutubeValidator
   def self.valid_url?(url)
-    url_regex = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
+    url_regex = /.*(?:https:\/\/www\.youtube\.com\/watch|https:\/\/www\.youtube\.com\/shorts\/).*/
     url_regex.match?(url)
   end
 end
